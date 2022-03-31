e12b0c875fa6ce505678e5f13beea2b5d75b335b
logan.dlpc <logan.dlpc@gmail.com>
2022-03-31 15:17:13 +0200

    Change Test

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,33 +6,33 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     redirect: '/tabs/Mes Commandes'
-  },
-  {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/Mes Commandes'
-      },
-      // {
-      //   path: 'tab1',
-      //   component: () => import('@/views/Tab1Page.vue')
-      // },
-      // {
-      //   path: 'tab2',
-      //   component: () => import('@/views/Tab2Page.vue')
-      // },
-      {
-        path: 'Mes Commandes',
-        component: () => import('@/views/Tab3Page.vue')
-      },
-      {
-        path: 'Commentaire',
-        component: () => import('@/views/Tab4Page.vue')
-      }
-    ]
   }
+  // {
+  //   path: '/tabs/',
+  //   component: TabsPage,
+  //   children: [
+  //     {
+  //       path: '',
+  //       redirect: '/tabs/Mes Commandes'
+  //     },
+  //     // {
+  //     //   path: 'tab1',
+  //     //   component: () => import('@/views/Tab1Page.vue')
+  //     // },
+  //     // {
+  //     //   path: 'tab2',
+  //     //   component: () => import('@/views/Tab2Page.vue')
+  //     // },
+  //     {
+  //       path: 'Mes Commandes',
+  //       component: () => import('@/views/Tab3Page.vue')
+  //     },
+  //     {
+  //       path: 'Commentaire',
+  //       component: () => import('@/views/Tab4Page.vue')
+  //     }
+  //   ]
+  // }
 ]
 
 const router = createRouter({
