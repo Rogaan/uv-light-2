6ea15c6d6bd6690fb721ea3dffba5e665901828e
logan.dlpc <logan.dlpc@gmail.com>
2022-04-14 08:49:22 +0200

    Modif le 14/04/2022 à 9H

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,28 +2,79 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Mes Commandes</ion-title>
+        <ion-title>Page de Connexion</ion-title>
       </ion-toolbar>
     </ion-header>
+
+    <!-- CONTENU DE TOUTE LA PAGE -->
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Page de Connexion</ion-title>
+          
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Mes Commandes" />
+      <ion-item id="input_login">
+          <ion-label position="floating">Login</ion-label>
+          <ion-input v-model="login"/>
+    </ion-item>
+          
+      <ion-item id="input_mdp">
+          <ion-label position="floating">Mot de passe</ion-label>
+          <ion-input  v-model="mdp"/>
+      </ion-item>
+
+      <ion-button id="bouton_connexion" color="success" @click="RecupInput">Connexion</ion-button>
+
+      
+      <!-- TEST -->
+      
+      <!-- FIN DE TEST -->
     </ion-content>
   </ion-page>
+  
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonInput, IonItem, IonButton  } from '@ionic/vue';
 
+//import ExploreContainer from '@/components/ExploreContainer.vue';
 export default  defineComponent({
-  name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  data() {
+    return {
+      login: "",
+      mdp: ""
+    }
+  },
+  name: 'Login1User',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonLabel, IonInput, IonItem, IonButton },
+  methods: { 
+    RecupInput() {
+      alert(this.login + " - " + this.mdp)
+    },
+  },
 });
+
 </script>
+<!-- STYLE UNIQUEMENT POUR CETTE PAGE -->
+<style scoped>
+  #bouton_connexion {
+    display: block;
+    margin : auto;
+    margin-top: 20px;
+    width: 40%;
+  }
+
+  #input_mdp{
+    width: 70%;
+    margin : auto;
+    margin-top: 10px;
+  }
+
+  #input_login{
+    width: 70%;
+    margin : auto;
+    margin-top: 200px;
+  }
+</style>
